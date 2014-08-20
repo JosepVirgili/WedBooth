416b6b3dcda0298cf2f2f65b3524e920f4a1d2dd
JosepVirgili <josep.virgili.llop@gmail.com>
2014-08-20 16:33:30 +0100

    Add fucntion that takes picture with the DSLR.

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from subprocess import call
 
 #GPIO configuration
 GPIO.setmode(GPIO.BOARD) #Set the Board Mode
@@ -13,11 +14,16 @@ def WakeUpDSLR():
 	GPIO.output(7, True) #Wake up camera
 	time.sleep(1) #Sleep for 1 second to give the camera time to wake up
 	GPIO.output(7, False) #Stop waking the camera
+
+def TakePicDSLR():
+	#Takes a picture with the DSLR camera
+	call('gphoto2 --capture-image',shell=True)
+	
 	
 #Main script
 WakeUpDSLR()
 time.sleep(5) #Some time is required for the camera to come online
-WakeUp()	
+TakePicDSLR()	
 	
 #Clean up	
 GPIO.cleanup() #Clean up of the GPIO Port

f31e9a3617f5a23e4303732e576ee7a6af95c693
JosepVirgili <josep.virgili.llop@gmail.com>
2014-08-20 15:18:39 +0100

    Added function that wakes-up DSLR camera.

---
+++ PhotoBooth.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+#GPIO configuration
+GPIO.setmode(GPIO.BOARD) #Set the Board Mode
+GPIO.setup(7, GPIO.OUT) #Sets up GPIO Pin 7 to Output
+
+
+def WakeUp():
+	#Wakes up the DSLR camera
+	GPIO.output(7, True) #Wake up camera
+	time.sleep(1) #Sleep for 1 second to give the camera time to wake up
+	GPIO.output(7, False) #Stop waking the camera
+	
+#Main script
+WakeUp()	
+	
+#Clean up	
+GPIO.cleanup() #Clean up of the GPIO Port
+	
+	
+
+	
+	
+	

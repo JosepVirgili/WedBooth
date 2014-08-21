840f0d617f067f36b9c4599edc8ceb908c5e2f52
JosepVirgili <josep.virgili.llop@gmail.com>
2014-08-21 14:25:22 +0100

    The image taken by the camera is now downloaded into the disk. Added a function that displays and image from the disk into the display using pygame.

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -1,31 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 from subprocess import call
+import pygame
 
 #GPIO configuration
 GPIO.setmode(GPIO.BOARD) #Set the Board Mode
 GPIO.setup(7, GPIO.OUT) #Sets up GPIO Pin 7 to Output
 GPIO.output(7, False) #Not wake the camera yet
 
+#Pygame configuration
+pygame.init()
+screenSize = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+pygame.mouse.set_visible(0)
+pygame.display.set_mode(screenSize, pygame.FULLSCREEN)
+screen = pygame.display.set_mode(screenSize)
 
-
+#--- Functions ---#
 def WakeUpDSLR():
 	#Wakes up the DSLR camera
 	GPIO.output(7, True) #Wake up camera
 	time.sleep(1) #Sleep for 1 second to give the camera time to wake up
 	GPIO.output(7, False) #Stop waking the camera
 
-def TakePicDSLR():
-	#Takes a picture with the DSLR camera
-	call('gphoto2 --capture-image',shell=True)
+def TakePicDSLR(filename):
+	#Takes a picture with the DSLR camera and downloads it to the RPi saving it with the specified filename
+	call('gphoto2 --capture-image-and-download --filename '+filename+' --keep --force-overwrite',shell=True)
+
+def DisplayImage(filename):
+	#Diplays and image from file into the display
+	image=pygame.image.load(filename) #Load image
+	image = pygame.transform.scale(image.convert(), screenSize) #Resizes it to fit screen
+	#Diplays image in siplay
+	screen.blit (image, (0,0))
+	pygame.display.update()
 	
 	
-#Main script
+#--- Main script ---#
+filename = 'WedBoothPic.jpg' #Filename of the image
 WakeUpDSLR()
 time.sleep(5) #Some time is required for the camera to come online
-TakePicDSLR()	
+TakePicDSLR(filename)
+DisplayImage(filename)
+time.sleep(5) #Time to appreciate the image taken.
 	
-#Clean up	
+#--- Clean up ---#	
 GPIO.cleanup() #Clean up of the GPIO Port
 	
 	

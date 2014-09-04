a67c11ecd78c0d14eafae353d2d39c0a4e657da7
JosepVirgili <josep.virgili.llop@gmail.com>
2014-09-04 21:00:40 +0100

    Take image from PiCam and diplay it while the other is loading. Added threading for extra speed.

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -4,6 +4,8 @@ from subprocess import call
 import pygame
 import picamera
 import math
+import io
+from threading import Thread
 
 #GPIO configuration
 GPIO.setmode(GPIO.BOARD) #Set the Board Mode
@@ -23,11 +25,16 @@ background.fill((0, 0, 0))
 
 #Initialize Camera
 camera = picamera.PiCamera()
-camera.resolution = (1296,730)
-camera.framerate = 30
+cam_resolution = (640,480)
+camera.resolution = cam_resolution
+camera.framerate = 15
 camera.start_preview()
 preview_alpha = 200 #Nominal alpha value
 camera.preview_alpha = preview_alpha
+
+#Configure DSLR camera
+WakeUpDSLR()
+call('gphoto2 --set-config imageformat=7')
 
 #Set countdown text font
 font = pygame.font.SysFont("arial", screenSize[1])
@@ -36,22 +43,39 @@ font = pygame.font.SysFont("arial", screenSize[1])
 def WakeUpDSLR():
 	#Wakes up the DSLR camera
 	GPIO.output(7, True) #Wake up camera
-	time.sleep(1) #Sleep for 1 second to give the camera time to wake up
+	time.sleep(2) #Sleep for 2 second to give the camera time to wake up
 	GPIO.output(7, False) #Stop waking the camera
 
 def TakePicDSLR(filename):
 	#Takes a picture with the DSLR camera and downloads it to the RPi saving it with the specified filename
 	call('gphoto2 --capture-image-and-download --filename '+filename+' --keep --force-overwrite',shell=True)
 
-
+def TakePicPiCamStream(camera,cam_resolution):
+	#Takes a picture with the Pi camera and returns it
+	stream = io.BytesIO() #Create stream
+	camera.capture(stream, 'rgb') #Capture picture
+	rgb = bytearray(cam_resolution[0] * cam_resolution[1] * 3) 	# Buffers for PiCamera picture
+	#Read picture into buffer
+	stream.seek(0)
+	stream.readinto(rgb)
+	#Return the image
+	return pygame.image.frombuffer(rgb[0:cam_resolution[0]*cam_resolution[1]*3],cam_resolution, 'RGB')
 
 def DisplayImageFile(filename):
 	#Diplays and image from file into the display
 	image=pygame.image.load(filename) #Load image
 	image = pygame.transform.scale(image.convert(), screenSize) #Resizes it to fit screen
-	#Diplays image in siplay
+	#Diplays image in display
 	screen.blit (image, (0,0))
 	pygame.display.update()
+
+def DisplayImagePi(image):
+	#Display image from an image object
+	image = pygame.transform.scale(image.convert(), screenSize) #Resizes it to fit screen
+	#Diplays image in display
+	screen.blit (image, (0,0))
+	pygame.display.update()
+
 
 def Countdown(count,background):
 	#Performs countdown with preview
@@ -73,10 +97,15 @@ def Countdown(count,background):
 	
 #--- Main script ---#
 filename = 'WedBoothPic.jpg' #Filename of the image
-WakeUpDSLR()
+t_PicDSLR = Thread(target=TakePicDSLR,args=(filename,))
+t_WakeUp = Thread(target=WakeUpDSLR,args=())
+t_WakeUp.start()
 Countdown(5,background)
-TakePicDSLR(filename)
+t_PicDSLR.start()
+img = TakePicPiCamStream(camera,cam_resolution)
 camera.preview_alpha = 0
+DisplayImagePi(img)
+t_PicDSLR.join()
 DisplayImageFile(filename)
 time.sleep(5) #Time to appreciate the image taken.
 	
